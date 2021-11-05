a1b56e24508704c6d55e7539524e6fb6b2a60833
lukemclennan <lukemclennan9@gmail.com>
2021-11-05 17:12:05 -0500

    Added Order.cs, OrderItem.cs, Cart.cs, CartItem.cs, and User.cs, with their attributes. Methods are defined but not yet implemented

---
+++ SoftwareEngineeringFinalProject/SoftwareEngineeringFinalProject/Models/Cart.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareEngineeringFinalProject.Models
+{
+    public class Cart
+    {
+        [PrimaryKey, AutoIncrement]
+        public int CartID { get; set; }
+        
+        public void AddToCart(CartItem item)
+        {
+
+        }
+    }
+}
---
+++ SoftwareEngineeringFinalProject/SoftwareEngineeringFinalProject/Models/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareEngineeringFinalProject.Models
+{
+    public class OrderItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int OrderItemID { get; set; }
+        public int OrderID { get; set; }
+        public int FlowerArrangementID { get; set; }
+
+        public void AddToOrder(Order order)
+        {
+
+        }
+
+    }
+}
