50997d581799ec74772c32acf437092483008ae3
ep943169 <93725253+ep943169@users.noreply.github.com>
2021-11-08 16:40:13 -0600

    Add files via upload

---
+++ SoftwareEngineeringFinalProject/SoftwareEngineeringFinalProject/UserModeTabbedPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+
+namespace SoftwareEngineeringFinalProject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserModeTabbedPage : Xamarin.Forms.TabbedPage
+    {
+        public UserModeTabbedPage()
+        {
+            InitializeComponent();
+            On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+        }
+    }
+}
