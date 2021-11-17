37de9ab8a2ac853c9e9c94b4cada4b388e12b6c5
ep943169 <93725253+ep943169@users.noreply.github.com>
2021-11-17 14:55:38 -0600

    Update AdminLoginPage.xaml.cs

--- SoftwareEngineeringFinalProject/SoftwareEngineeringFinalProject/AdminLoginPage.xaml.cs
+++ SoftwareEngineeringFinalProject/SoftwareEngineeringFinalProject/AdminLoginPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using SoftwareEngineeringFinalProject.Models;
+using System;
 using System.Collections.Generic;
 
 using Xamarin.Forms;
@@ -7,9 +8,44 @@ namespace SoftwareEngineeringFinalProject
 {
     public partial class AdminLoginPage : ContentPage
     {
+        public int errorCount = 0;
+        public string username;
+        public string password;
         public AdminLoginPage()
         {
             InitializeComponent();
+        }
+        public void ValidateLogin()
+        {
+            if (Entry_Username.Text == null)
+                errorCount++;
+            if (Entry_Password.Text == null)
+                errorCount++;
+        }
+        public async void LoginProcedure(object sender, EventArgs e)
+        {
+            password = Entry_Password.Text;
+            username = Entry_Username.Text;
+
+            ValidateLogin();
+            if (errorCount > 0) //one of the entries is empty
+            {
+                await DisplayAlert("Error", "One or Both Entry Fields are empty", "Ok");
+                errorCount = 0;
+            }
+            else //both entries are not empty
+            {
+                Admin admin = await App.DB.GetAdminAsync(username, password);
+                if (admin == null)
+                {
+                    await DisplayAlert("Error", "One or Both Entry Fields are invalid", "Ok");
+                }
+                else
+                {
+                    await Navigation.PushAsync(new UserModeTabbedPage());
+                }
+                //await DisplayAlert("Test", "Username: " + username + " Password: " + password, "Ok");
+            }
         }
     }
 }
