382a6bcbc49808996938a85f7fd9d4e546676576
ep943169 <93725253+ep943169@users.noreply.github.com>
2021-11-17 15:06:01 -0600

    Update Admin.cs

--- SoftwareEngineeringFinalProject/SoftwareEngineeringFinalProject/Models/Admin.cs
+++ SoftwareEngineeringFinalProject/SoftwareEngineeringFinalProject/Models/Admin.cs
@@ -1,10 +1,19 @@
-﻿using System;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
 namespace SoftwareEngineeringFinalProject.Models
 {
     public class Admin
     {
-        public Admin()
-        {
-        }
+        [PrimaryKey, AutoIncrement]
+        public int AdminID { get; set; }
+        public string AdminFirstName { get; set; }
+        public string AdminLastName { get; set; }
+        public string AdminUsername { get; set; }
+        public string AdminPassword { get; set; }
+
     }
 }
+
