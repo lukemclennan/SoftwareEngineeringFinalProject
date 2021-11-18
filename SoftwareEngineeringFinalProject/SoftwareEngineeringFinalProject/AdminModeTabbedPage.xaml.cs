96663341cf0db3c07dc8305d9464dc4658789556
alexisM8 <75651185+alexisM8@users.noreply.github.com>
2021-11-18 11:18:38 -0600

    Add files via upload

---
+++ SoftwareEngineeringFinalProject/SoftwareEngineeringFinalProject/AdminModeTabbedPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SoftwareEngineeringFinalProject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AdminModeTabbedPage : TabbedPage
+    {
+        public AdminModeTabbedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
