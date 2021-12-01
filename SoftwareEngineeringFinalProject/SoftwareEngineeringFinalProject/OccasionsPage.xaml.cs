83b1bcd2f0fe0d68990d0a24c79a991ea17c6f03
ep943169 <93725253+ep943169@users.noreply.github.com>
2021-12-01 16:05:04 -0600

    Add files via upload

---
+++ SoftwareEngineeringFinalProject/SoftwareEngineeringFinalProject/OccasionsPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SoftwareEngineeringFinalProject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OccasionsPage : ContentPage
+    {
+        public OccasionsPage()
+        {
+            InitializeComponent();
+            this.Title = "Occasions";
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            collectionView.ItemsSource = await App.DB.GetOccasionsAsync();
+
+        }
+    }
+}
