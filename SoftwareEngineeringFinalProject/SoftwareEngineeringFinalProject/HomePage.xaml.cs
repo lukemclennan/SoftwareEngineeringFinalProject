a06dc20e53d243f0fabef1e7e075938b8e361fdb
lukemclennan <86444016+lukemclennan@users.noreply.github.com>
2021-12-01 15:52:47 -0600

    Add files via upload

---
+++ SoftwareEngineeringFinalProject/SoftwareEngineeringFinalProject/HomePage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using SoftwareEngineeringFinalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SoftwareEngineeringFinalProject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+
+            this.Title = "Home";
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            collectionView.ItemsSource = await App.DB.GetFlowersAsync();
+        }
+
+        private async void SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int flowerID = ((Flower)e.CurrentSelection.FirstOrDefault()).FlowerID;
+            await Navigation.PushAsync(new ViewArrangements(flowerID));
+            //bool addToCart = await DisplayAlert("Add to Cart?", "Would you like to add " + flower.FlowerName + " to your cart?", "Yes", "No");
+            //if (addToCart)
+            //{
+            //    FlowerArrangement flowerArrangement = new FlowerArrangement {
+            //        FlowerID = flower.FlowerID,
+            //        FlowerArrangementName = flower.FlowerName + " Arrangement"
+            //    };
+            //    await App.DB.SaveFlowerArrangementAsync(flowerArrangement);
+            //    CartItem cartItem = new CartItem { 
+            //        CartID = App.User.CartID,
+            //        FlowerArrangementID = flowerArrangement.FlowerArrangementID
+            //    };
+            //    //await DisplayAlert("FlowerArrangmentID", flowerArrangement.FlowerArrangementID.ToString(), "Ok");
+            //    await App.DB.SaveCartItemAsync(cartItem);
+            //    await DisplayAlert("Success", "Item added to cart.", "Ok");
+            //}
+        }
+    }
+}
