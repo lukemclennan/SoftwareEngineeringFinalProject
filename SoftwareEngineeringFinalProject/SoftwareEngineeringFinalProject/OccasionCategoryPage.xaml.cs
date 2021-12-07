008734a013f0e834bc34d7ba7c305706d25aff51
ep943169 <93725253+ep943169@users.noreply.github.com>
2021-12-06 20:31:31 -0600

    Add files via upload

---
+++ SoftwareEngineeringFinalProject/SoftwareEngineeringFinalProject/OccasionCategoryPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftwareEngineeringFinalProject.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SoftwareEngineeringFinalProject
+{
+    public partial class OccasionCategoryPage : ContentPage
+    {
+        public string occCategory;
+
+        public OccasionCategoryPage(string category)
+        {
+            occCategory = category;
+
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            collectionViews.ItemsSource = await App.DB.GetOccasionByCategory(occCategory);
+
+
+        }
+    }
+}
